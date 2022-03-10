53103aec69ad95567f804f7cc1a502336f8651b4
Peerawich Sinamornwech <sunpeerawich@gmail.com>
2022-03-10 21:32:13 +0700

    Finish Movement System
    Missing Collider layerMask
    Missing Animation for Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Reference Video: https://youtu.be/mbzXIOKZurA
+    // Reference Video:
+    // Grid Based Movement in Unity: https://youtu.be/mbzXIOKZurA
 
+    // Don't forget to reference!!!
     public float moveSpeed = 5f;
     public Transform movePoint;
+
+    public LayerMask whatStopsMovement;
+
+    public Animator anim;
+
+    /////////////////////////////////////////////////////////
 
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;    // movePoint child won't have parent any more.
+                                    // Why? idk.
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime); // Vector3.MoveTowards(current position, target position, max distance delta?)
+        //Debug.Log("Current Position: " + transform.position);
+        Debug.Log("Target Position: " + movePoint.position);
 
-        // "Horizontal" && "Vertical" string used below are from Unity's
+        transform.position = Vector3.MoveTowards(/*current pos*/transform.position, /*target pos*/movePoint.position, /*max distance delta*/moveSpeed * Time.deltaTime); 
 
-        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) // Button Check Horizontal // Get Raw Axis for Horizontal Input, use absolute on it so that it is "1" no matter what, then check if it is equal to "1".
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f) // Check for distance between "current pos" and "target pos", checks if it's <= .05f
         {
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            // "Horizontal" && "Vertical" string used below are from Unity's
+            // Get Raw Axis for Horizontal Input, use absolute on it so that it is "1" no matter what, then check if it is equal to "1".
+            // Use "else if" to combine the 2 Button Checks if you don't want the player to walk diagonally.
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) // Button Check Horizontal 
+            {
+                // Checks for Colliders
+                if(!Physics2D.OverlapCircle(/*vector*/movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),/*circle radius*/.2f, /*layer mask*/whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) // Button Check Vertical
+            {
+                // Checks for Colliders
+                if (!Physics2D.OverlapCircle(/*vector*/movePoint.position + new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f),/*circle radius*/.2f, /*layer mask*/whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+
+            anim.SetBool("Moving", false); 
         }
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) // Button Check Vertical
+        else
         {
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            anim.SetBool("Idle", true);
         }
     }
 }
